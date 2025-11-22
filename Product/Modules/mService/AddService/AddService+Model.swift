7896dccb81412febfbc6ac702ab7557c795a4d88
d.molkov <d.molkov@fbs.com>
2025-11-22 16:18:10 +0100

    add history

---
+++ Product/Modules/mService/AddService/AddService+Model.swift
@@ -0,0 +1,7 @@
+//
+//  addService+Model.swift
+//  myMotor
+//
+//  Created by Denis Molkov on 27.10.2025.
+//
+
---
+++ Product/Modules/mService/AddService/AddService+View.swift
@@ -0,0 +1,7 @@
+//
+//  addService+View.swift
+//  myMotor
+//
+//  Created by Denis Molkov on 27.10.2025.
+//
+
